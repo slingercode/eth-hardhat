69f9d9cc86b6812485497ff271cbd2bbcbc076b0
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2021-10-09 22:16:57 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Index = () => (
+  <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>slinger.eth</main>
+  </>
+);
+
+export default Index;
